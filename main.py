9c4be5bc75bf86cd1e4970effc1001d728f7bd05
SvyatoslavK <s.kovylskiy@yandex.ru>
2020-06-09 23:16:05 +0500

    Added functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-def get_capacity(T, P, U=12):
+def get_capacity(T, P, U=48):
     """
     принимает время в часах и нагрузку в кВт
     возвращает емкость АКБ для 12Вольт
@@ -22,10 +22,10 @@ def get_work_time(C_bat, N, P):
     """
     C_gen = C_bat * N
     U = 12
-    T = (U * get_K()) / (C_gen * P*1000 )
+    T = (U * get_K() * C_gen  ) / ( P*1000 )
     return T
 
-def get_K(K=0.99, K_gr=0.8, K_de = 0.9 ):
+def get_K(K=0.9, K_gr=0.7, K_de = 0.8 ):
     """
     Принимает коэфициенты такие как
     K - КПД инвертора
@@ -44,4 +44,5 @@ def main():
     print("Емкость одной батареи должна быть: {:02.2f}".format(C_bat))
 
 if __name__ == "__main__":
-    main()
+    print("{:02.2f}".format(get_work_time(100, 8, 0.6)))
+    print("{:02.2f}".format(get_capacity(4, 8), 48))
